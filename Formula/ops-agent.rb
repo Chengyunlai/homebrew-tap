8ad72daac79837618a1517bf7bcb831237a139a6
yun <58059527+Chengyunlai@users.noreply.github.com>
2026-08-03 06:29:52 +0000

    chore: update ops-agent to 0.1.7

--- Formula/ops-agent.rb
+++ Formula/ops-agent.rb
@@ -1,22 +1,22 @@
 class OpsAgent < Formula
   desc "Kubernetes operations agent terminal"
   homepage "https://github.com/Chengyunlai/ops-agent"
-  version "0.1.6"
+  version "0.1.7"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Chengyunlai/ops-agent/releases/download/v0.1.6/ops-agent_0.1.6_darwin-arm64.tar.gz"
-      sha256 "9196f0a6ad4d4e5d80e6d7cd9ae7472823d19ef3178f2c6e2e10ea87ee11bebb"
+      url "https://github.com/Chengyunlai/ops-agent/releases/download/v0.1.7/ops-agent_0.1.7_darwin-arm64.tar.gz"
+      sha256 "7d399c5e6a13985eb4597de05f8ddfb6de08ac1076bb156bd7965e42e015cb87"
     else
-      url "https://github.com/Chengyunlai/ops-agent/releases/download/v0.1.6/ops-agent_0.1.6_darwin-amd64.tar.gz"
-      sha256 "ba3a90436764bbcf55639d14ed3a316e908f972544312ec2c56efb64a2debc74"
+      url "https://github.com/Chengyunlai/ops-agent/releases/download/v0.1.7/ops-agent_0.1.7_darwin-amd64.tar.gz"
+      sha256 "2e88bab99ba90d45501f1dba0b54dc969b187d46d9b0adbdc2f537e58b94bfe7"
     end
   end
 
   on_linux do
-    url "https://github.com/Chengyunlai/ops-agent/releases/download/v0.1.6/ops-agent_0.1.6_linux-amd64.tar.gz"
-    sha256 "46dc2837d119339b748cd48a73d6b0bb3131a3d52414e7bcfc792f5df6048a8b"
+    url "https://github.com/Chengyunlai/ops-agent/releases/download/v0.1.7/ops-agent_0.1.7_linux-amd64.tar.gz"
+    sha256 "464cef5375cd1bba580215c32f64e96b206e248a5550980db1f23af5f6c0540a"
     depends_on arch: :x86_64
   end
 
